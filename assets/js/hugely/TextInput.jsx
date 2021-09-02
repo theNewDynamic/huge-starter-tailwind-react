fdc0f0de9cd468f2f1c6ab95c7b958f29c0f0eef
Regis Philibert <login@regisphilibert.com>
2021-09-02 16:39:02 -0400

    Fix input text color in darkMode

--- assets/js/hugely/TextInput.jsx
+++ assets/js/hugely/TextInput.jsx
@@ -9,7 +9,7 @@ export default class TextInput extends Component {
   render() {
     return (
       <input 
-        className="sm:absolute mt-px text-center top-24 rounded md:top-32 left-1/2 sm:transform sm:-translate-x-1/2 w-40 md:w-52 border-2 border-black text-xs md:text-base p-1 z-10"
+        className="sm:absolute mt-px text-center text-black top-24 rounded md:top-32 left-1/2 sm:transform sm:-translate-x-1/2 w-40 md:w-52 border-2 border-black text-xs md:text-base p-1 z-10"
         placeholder={params.placeholder} onChange={this.handleChange} />
     );
   }
